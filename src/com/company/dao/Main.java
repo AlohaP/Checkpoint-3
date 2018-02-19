bbb6049520cd51242fe06c0bbe6d6e1bcaa1ce09
Piotr Górski <piotrek.gorski90@gmail.com>
2018-02-19 10:15:25 +0100

    Add connect to database class, and main to test it

---
+++ src/com/company/dao/Main.java
@@ -0,0 +1,11 @@
+package com.company.dao;
+
+import java.sql.Connection;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DatabaseConnection database = DatabaseConnection.getInstance();
+        Connection connection = null;
+    }
+}
