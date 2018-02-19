930d0a4bcf8d65528e791bd603017a375610cf6d
Piotr Górski <piotrek.gorski90@gmail.com>
2018-02-19 10:41:49 +0100

    Add template to Book with getters/setters

---
+++ src/com/company/model/Book.java
@@ -0,0 +1,70 @@
+package com.company.model;
+
+public class Book {
+    private int ISBN;
+    private String author;
+    private String title;
+    private String publisher;
+    private String publication_year;
+    private int year;
+    private String type;
+
+    public Book(){
+
+    }
+
+    public int getISBN(){
+        return ISBN;
+    }
+     public void setISBN(int ISBN){
+        this.ISBN = ISBN;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublication_year() {
+        return publication_year;
+    }
+
+    public void setPublication_year(String publication_year) {
+        this.publication_year = publication_year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
